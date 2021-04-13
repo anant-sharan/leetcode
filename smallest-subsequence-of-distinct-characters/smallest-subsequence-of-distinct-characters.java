fdc7a26f084c6ea96d48bf46b81f76e6e2512ee1
anant-sharan <52694418+anant-sharan@users.noreply.github.com>
2021-04-13 21:49:02 +0530

    Time: 2 ms (95.35%), Space: 37.3 MB (40.29%) - LeetHub

---
+++ smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Solution {
+    public String smallestSubsequence(String s) {
+        int lst[]=new int[26];
+        int vis[]=new int[26];
+        int i,l=s.length();
+        char ch;
+        for(i=0;i<l;i++)
+        {
+            ch=s.charAt(i);
+            lst[ch-'a']=i;    
+        }
+        Stack<Character> st=new Stack<Character>();
+        for(i=0;i<l;i++)
+        {
+            ch=s.charAt(i);
+            if(vis[ch-'a']==1)
+                continue;
+            if(st.empty())
+            {
+                st.push(ch);
+                vis[ch-'a']=1;
+            }
+            else if(st.peek()>ch && lst[st.peek()-'a']>i)
+            {
+                while(st.empty()==false && st.peek()>ch && lst[st.peek()-'a']>i)
+                {
+                    vis[st.peek()-'a']=0;
+                    st.pop();
+                }
+                st.push(ch);
+                vis[ch-'a']=1;
+            }
+            else
+            {
+                st.push(ch);
+                vis[ch-'a']=1;
+            }
+        }
+        String str="";
+        while(st.empty()==false)
+        {
+            str=st.pop()+str;
+        }
+        return str;
+    }
+}
