68ab5e812a5c4179877ca82217514449c8c53efa
anant-sharan <52694418+anant-sharan@users.noreply.github.com>
2021-07-01 16:33:35 +0530

    Time: 21 ms (41.89%), Space: 41.4 MB (66.22%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.java
+++ subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,23 +1,14 @@
 import java.util.*;
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int l=nums.length,i,c=0,sum=0;
-        if(l==1)
-        {
-            if(nums[0]!=k)
-                return 0;
-            else
-                return 1;
-        }
-        Hashtable<Integer,Integer> h= new Hashtable<Integer,Integer>();
+        int i,l=nums.length,c=0,sum=0;
+        Hashtable<Integer,Integer> h=new Hashtable<Integer,Integer>();
         h.put(0,1);
         for(i=0;i<l;i++)
         {
             sum=sum+nums[i];
             if(h.containsKey(sum-k))
-            {
                 c=c+h.get(sum-k);
-            }
             if(h.containsKey(sum))
                 h.put(sum,h.get(sum)+1);
             else
